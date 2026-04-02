ab5c27c7e60d0d37b628035396dd313f261fa247
ahmadmiftianur@gmail.com <ahmadmiftianur@gmail.com>
2026-04-02 11:09:11 +0700

    menambahkan logika penyelesaian pada fungsi mergesort

--- merge sort/mergesort.cpp
+++ merge sort/mergesort.cpp
@@ -66,4 +66,19 @@ void mergesort(int low, int high)
             j++;           // step 4.d.ii.2
         }
         k++; // step 4.d.iii
+    }
+    while (j <= high) // step 4.e
+    {
+        B[k] = arr[j]; // step 4.e.i
+        j++;           // step 4.e.ii
+        k = k + 1;     // step 4.e.iii
+    }
+    
+    while (i <= mid) // step 4.f
+    {
+        B[k] = arr[i]; // step 4.f.i
+        i++;           // step 4.f.ii
+        k++;     // step 4.f.iii
+    }
+
     
